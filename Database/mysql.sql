55e4bf9da255e3362d088b76ba1813d7f8057b85
Dixit8112 <77315639+Dixit8112@users.noreply.github.com>
2024-12-06 22:10:59 -0500

    Created mysql.sql file for database

---
+++ Database/mysql.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS employees;
+USE employees;
+
+CREATE TABLE IF NOT EXISTS employee(
+	  Emp_id VARCHAR(20), 
+	  First_name VARCHAR(20), 
+	  Last_name VARCHAR(20), 
+	  primary_skill VARCHAR(50), 
+	  location VARCHAR(20) 
+	);
+
+	INSERT INTO employee VALUES ('1', 'Sarthak', 'Shah', 'java', 'mars');
+	INSERT INTO employee VALUES ('2', 'darshan', 'modi', '.net', 'sun');
+	INSERT INTO employee VALUES ('3', 'dixit', 'italiya', 'data', 'saturn');
+
+	SELECT * FROM employee;
